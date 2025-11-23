c0a7e2cdcfc29287a17c8f20e84f5f3eeebd1d72
jhcode33 <jhcode33@gmail.com>
2025-11-23 15:38:14 +0900

    feat(server): Add HttpRequest

---
+++ src/main/java/server/HttpRequest.java
@@ -0,0 +1,44 @@
+package server;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpRequest {
+
+    private String method;
+    private String path;
+    private String version;
+    private Map<String, String> headers = new HashMap<>();
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public void addHeader(String key, String value) {
+        this.headers.put(key, value);
+    }
+}
