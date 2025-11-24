5c470a7a1123665a398ce3d8b8d06bbe9dc91ae6
jhcode33 <jhcode33@gmail.com>
2025-11-24 16:34:02 +0900

    feat(spring): Add RequestMapping annotation

---
+++ src/main/java/spring/annotation/RequestMapping.java
@@ -0,0 +1,9 @@
+package spring.annotation;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface RequestMapping {
+    String value();
+}
