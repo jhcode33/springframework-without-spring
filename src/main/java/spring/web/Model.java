448abed9b7715d36c593df0fc21980ce3ee213d7
jhcode33 <jhcode33@gmail.com>
2025-11-24 18:53:12 +0900

    feat(web): Add Model

---
+++ src/main/java/spring/web/Model.java
@@ -0,0 +1,22 @@
+package spring.web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Model {
+
+    private final Map<String, Object> attributes = new HashMap<>();
+
+    public void addAttribute(String key, Object value) {
+        attributes.put(key, value);
+    }
+
+    public Object getAttribute(String key) {
+        return attributes.get(key);
+    }
+
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+}
+
